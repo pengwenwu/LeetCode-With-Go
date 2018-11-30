cac37438db8c9a333ad557f964de8dc15d79e45e
pengwenwu <pww932589183@163.com>
2018-11-30 08:24:02 +0800

    add:

--- kit/01_array/array.go
+++ kit/01_array/array.go
@@ -5,19 +5,13 @@ import (
 	"fmt"
 )
 
-/**
- * 1) 数组的插入、删除、按照下标随机访问操作；
- * 2）数组中的数据是int类型的；
- *
- * Author: leo
- */
-
+// 定义
 type Array struct {
 	data   []int
 	length uint
 }
 
-//为数组初始化内存
+// 初始化内存
 func NewArray(capacity uint) *Array {
 	if capacity == 0 {
 		return nil
@@ -32,7 +26,7 @@ func (this *Array) Len() uint {
 	return this.length
 }
 
-//判断索引是否越界
+// 判定索引是否越界
 func (this *Array) isIndexOutOfRange(index uint) bool {
 	if index >= this.length {
 		return true
@@ -40,7 +34,7 @@ func (this *Array) isIndexOutOfRange(index uint) bool {
 	return false
 }
 
-//通过索引查找数组，索引范围[0,n-1]
+// 通过索引查找数组
 func (this *Array) Find(index uint) (int, error) {
 	if this.isIndexOutOfRange(index) {
 		return 0, errors.New("out of index range")
@@ -48,45 +42,18 @@ func (this *Array) Find(index uint) (int, error) {
 	return this.data[index], nil
 }
 
-//插入数值到索引index上
-func (this *Array) Insert(index uint, v int) error {
+// 在指定索引位置插入数据
+func (this *Array) InsertToIndex(index uint, v int) error {
 	if this.Len() == uint(cap(this.data)) {
 		return errors.New("full array")
 	}
 	if index != this.length && this.isIndexOutOfRange(index) {
-		return errors.New("out of index range")
+		return errors.New("out of range")
 	}
-
 	for i := this.length; i > index; i-- {
 		this.data[i] = this.data[i-1]
 	}
 	this.data[index] = v
 	this.length++
 	return nil
-}
-
-func (this *Array) InsertToTail(v int) error {
-	return this.Insert(this.Len(), v)
-}
-
-//删除索引index上的值
-func (this *Array) Delete(index uint) (int, error) {
-	if this.isIndexOutOfRange(index) {
-		return 0, errors.New("out of index range")
-	}
-	v := this.data[index]
-	for i := index; i < this.Len()-1; i++ {
-		this.data[i] = this.data[i+1]
-	}
-	this.length--
-	return v, nil
-}
-
-//打印数列
-func (this *Array) Print() {
-	var format string
-	for i := uint(0); i < this.Len(); i++ {
-		format += fmt.Sprintf("|%+v", this.data[i])
-	}
-	fmt.Println(format)
 }

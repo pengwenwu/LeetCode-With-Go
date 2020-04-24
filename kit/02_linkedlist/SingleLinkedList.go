59bf503db6a47b63ff8318f5a5a39c7033df5465
pengwenwu <pengwenwu@corp-ci.com>
2020-04-24 19:00:32 +0800

    update:linkedlist重写

--- kit/02_linkedlist/SingleLinkedList.go
+++ kit/02_linkedlist/SingleLinkedList.go
@@ -1,11 +1,12 @@
 package LinkedList
 
 import (
+	"errors"
 	"fmt"
 )
 
 /*
-- 基本
+- 基本(单链表)
 	头插法
 	尾插法
 	在某个节点前插入
@@ -17,334 +18,341 @@ import (
 	单链表反转
 	回文字符串判定
 	链表中环的检测
+	两个有序的链表合并
+	删除链表倒数第 n 个结点
+	求链表的中间结点
 	在一个有环链表中，如何找出链表的入环点？以及环长
 	判断两个单向链表是否相交，如果相交，求出交点
 */
 
+// 单链表节点
 type ListNode struct {
-	next  *ListNode
 	value interface{}
+	next  *ListNode
 }
 
+// 单链表
 type LinkedList struct {
 	head   *ListNode
 	length uint
 }
 
 func NewListNode(v interface{}) *ListNode {
-	return &ListNode{next: nil, value: v}
+	return &ListNode{v, nil}
 }
 
+// 带头节点链表（哨兵）
 func NewLinkedList() *LinkedList {
-	return &LinkedList{head: NewListNode(0), length: 0}
+	return &LinkedList{NewListNode(0), 0}
 }
 
-func (this *ListNode) GetNext() *ListNode {
-	return this.next
+func (l *ListNode) GetNext() *ListNode {
+	return l.next
 }
 
-func (this *ListNode) GetValue() interface{} {
-	return this.value
+func (l *ListNode) GetValue() interface{} {
+	if l == nil {
+		return errors.New("empty node")
+	}
+	return l.value
 }
 
-// 在某个节点前插入
-func (this *LinkedList) InsertBefore(p *ListNode, v *ListNode) bool {
-	if nil == p || p == this.head {
+// 在某个节点之前插入新的节点
+func (l *LinkedList) InsertBeforeNode(oldNode, newNode *ListNode) bool {
+	if oldNode == nil || oldNode == l.head {
 		return false
 	}
-	pre := this.head
-	cur := this.head.next
-	for nil != cur {
-		if cur == p {
+	pre := l.head
+	cur := l.head.next
+	for cur != nil {
+		if cur == oldNode {
 			break
 		}
 		pre = cur
 		cur = cur.next
 	}
-	if nil == cur {
+	// 未找到该节点
+	if cur == nil {
 		return false
 	}
-	newNode := NewListNode(v)
+	newNode.next = oldNode
 	pre.next = newNode
-	newNode.next = cur
-	this.length++
+	l.length++
 	return true
 }
 
-// 在某个节点后插入
-func (this *LinkedList) InsertAfter(p *ListNode, v interface{}) bool {
-	if nil == p {
+// 在某个节点之后插入新的节点
+func (l *LinkedList) InsertAfterNode(oldNode, newNode *ListNode) bool {
+	if oldNode == nil {
 		return false
 	}
-	newNode := NewListNode(v)
-	oldNext := p.next
-	p.next = newNode
-	newNode.next = oldNext
-	this.length++
+	newNode.next = oldNode.next
+	oldNode.next = newNode
+	l.length++
 	return true
 }
 
 // 头插法
-func (this *LinkedList) InsertToHead(v interface{}) bool {
-	return this.InsertAfter(this.head, v)
+func (l *LinkedList) InsertToHead(newNode *ListNode) bool {
+	return l.InsertAfterNode(l.head, newNode)
 }
 
 // 尾插法
-func (this *LinkedList) InsertToTail(v interface{}) bool {
-	cur := this.head
-	for nil != cur.next {
+func (l *LinkedList) InsertToTail(newNode *ListNode) bool {
+	cur := l.head
+	for cur.next != nil {
 		cur = cur.next
 	}
-	return this.InsertAfter(cur, v)
+	return l.InsertAfterNode(cur, newNode)
 }
 
-// 根据索引查找节点
-func (this *LinkedList) FindByIndex(index uint) *ListNode {
-	if index > this.length {
+// 通过索引查找节点
+func (l *LinkedList) FindNodeByIndex(index uint) *ListNode {
+	if index >= l.length {
 		return nil
 	}
-	cur := this.head.next
+	cur := l.head.next
 	for i := uint(0); i < index; i++ {
 		cur = cur.next
 	}
 	return cur
 }
 
-// 删除节点
-func (this *LinkedList) DeleteNode(p *ListNode) bool {
-	if nil == p {
-		return false
-	}
-	pre := this.head
-	cur := this.head.next
-	for nil != cur {
-		if cur == p {
+// 删除传入的节点
+func (l *LinkedList) DeleteNode(node *ListNode) bool {
+	pre := l.head
+	cur := l.head.next
+	for cur != nil {
+		if cur == node {
 			break
 		}
 		pre = cur
 		cur = cur.next
 	}
-	if nil == cur {
+	// 未查询到节点
+	if cur == nil {
 		return false
 	}
-	pre.next = p.next
-	p = nil
-	this.length--
+	pre.next = cur.next
+	node = nil
+	l.length--
 	return true
 }
 
-func (this *LinkedList) Print() {
-	cur := this.head.next
+// 打印链表
+func (l *LinkedList) Print() {
+	cur := l.head.next
 	format := ""
-	for nil != cur {
+	for cur != nil {
 		format += fmt.Sprintf("%+v", cur.GetValue())
 		cur = cur.next
-		if nil != cur {
+		if cur != nil {
 			format += "->"
 		}
 	}
 	fmt.Println(format)
 }
 
-// 判断是否是回文字符串
-// 思路一：找到中间点，翻转前半部分链表，时间复杂度O(n)
-func (this *LinkedList) IsPalindrome() bool {
-	switch this.length {
-	case 0:
-		return false
-	case 1:
-		return true
-	}
-	isPalindrome := true
-	step := this.length / 2
-	// 反转前半部分链表
-	var pre *ListNode = nil
-	cur := this.head.next
-	for i := uint(1); i <= step; i++ {
-		tmp := cur.next
-		cur.next = pre
-		pre = cur
-		cur = tmp
-	}
-	mid := cur
-
-	var left, right *ListNode = pre, nil
-	if this.length%2 != 0 {
-		right = mid.next
-	} else {
-		right = mid
-	}
-	for nil != left && nil != right {
-		if left.GetValue().(string) != right.GetValue().(string) {
-			isPalindrome = false
-			break
-		}
-		left = left.next
-		right = right.next
-	}
-
-	// 还原链表
-	cur = pre
-	pre = mid
-	for nil != cur {
-		tmp := cur.next
-		cur.next = pre
-		pre = cur
-		cur = tmp
-	}
-	this.head.next = pre
-	return isPalindrome
-}
-
-// 思路二：申请一个栈存储链表前半段，时间复杂度O(n)，空间复杂度O(n)
-func (this *LinkedList) isPalindrome2() bool {
-	len := this.length
-	switch len {
-	case 0:
-		return false
-	case 1:
-		return true
-	}
-	str := make([]string, 0, len/2)
-	cur := this.head
-	for i := uint(1); i <= len; i++ {
-		cur = cur.next
-		if len%2 != 0 && i == (len/2+1) { // 如果链表有奇数个节点，忽略中间节点
-			continue
-		}
-		if i <= len/2 { // 前半段入栈，后半段对比
-			str = append(str, cur.value.(string))
-		} else {
-			if str[len-i] != cur.value.(string) {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-// 翻转链表
-func (this *LinkedList) ReverseLinkedList() {
-	if nil == this.head || nil == this.head.next || nil == this.head.next.next {
-		return
-	}
-	pre := this.head.next
-	cur := this.head.next.next
-	pre.next = nil
-	for nil != cur {
-		tmp := cur.next
-		cur.next = pre
-		pre = cur
-		cur = tmp
-	}
-	this.head.next = pre
-}
-
-// 判断链表是否有环
-// 方法一： 双重遍历，时间复杂度O(n*n)，空间复杂度O(1)
-func (this *LinkedList) hasCycle() bool {
-	cur := this.head.next
-	for count := int(1); nil != cur; count++ {
-		new_cur := this.head.next
-		for new_count := 1; new_count < count; new_count++ {
-			if new_cur == cur {
-				return true
-			}
-			new_cur = new_cur.next
-		}
-		cur = cur.next
-	}
-	return false
-}
-
-// 方法二：HashSet存储，时间复杂度O(n)，空间复杂度O(n)
-
-// 方法三：快慢指针，时间复杂度O(n)，空间复杂度O(1)
-func (this *LinkedList) hasCycle2() bool {
-	slow := this.head.next
-	fast := this.head.next
-	for nil != fast && nil != fast.next {
-		slow = slow.next
-		fast = fast.next.next
-		if fast == slow {
-			return true
-		}
-	}
-	return false
-}
-
-// 获取有环链表入环点
-func (this *LinkedList) getEntryNodeOfLoop() interface{} {
-	slow := this.head.next
-	fast := this.head.next
-	for nil != fast && nil != fast.next {
-		slow = slow.next
-		fast = fast.next.next
-		if fast == slow {
-			break
-		}
-	}
-	// 相遇后慢指针返回链表头
-	slow = this.head.next
-	for slow != fast {
-		slow = slow.next
-		fast = fast.next
-	}
-	return slow
-}
-
-// 获取有环链表环长
-func (this *LinkedList) getCycleLengthOfLoop() uint {
-	slow := this.head.next
-	fast := this.head.next
-	for nil != fast && nil != fast.next {
-		slow = slow.next
-		fast = fast.next.next
-		if slow == fast {
-			break
-		}
-	}
-	slow = slow.next
-	fast = fast.next.next
-	var length uint = 1
-	for slow != fast {
-		slow = slow.next
-		fast = fast.next.next
-		length++
-	}
-	return length
-}
-
-// 判断两个链表是否相交，以及获取第一个交点
-// 分有环和无环
-func hasIntersection(linked_list1 *LinkedList, linked_list2 *LinkedList) interface{} {
-	// 一个有环，一个无环，必然不相交
-	// 如果都是无环，判断最后一个节点是否相同
-	if !linked_list1.hasCycle2() && !linked_list2.hasCycle2() {
-		end1 := linked_list1.head.next
-		end2 := linked_list2.head.next
-		for end1.next != nil {
-			end1 = end1.next
-		}
-		for end2.next != nil {
-			end2 = end2.next
-		}
-		if end1 == end2 {
-			return true
-		}
-		return false
-	} else if linked_list1.hasCycle2() && linked_list2.hasCycle2() {
-		// 都有环，必然是同一个环，则判断A中的入环点是否在B中出现过
-		entryNode := linked_list1.getEntryNodeOfLoop()
-		cur := linked_list2.head.next
-		for cur != nil {
-			if cur == entryNode {
-				return true
-			}
-			cur = cur.next
-		}
-		return false
-	} else {
-		return false
-	}
-}
+//
+//// 判断是否是回文字符串
+//// 思路一：找到中间点，翻转前半部分链表，时间复杂度O(n)
+//func (this *LinkedList) IsPalindrome() bool {
+//	switch this.length {
+//	case 0:
+//		return false
+//	case 1:
+//		return true
+//	}
+//	isPalindrome := true
+//	step := this.length / 2
+//	// 反转前半部分链表
+//	var pre *ListNode = nil
+//	cur := this.head.next
+//	for i := uint(1); i <= step; i++ {
+//		tmp := cur.next
+//		cur.next = pre
+//		pre = cur
+//		cur = tmp
+//	}
+//	mid := cur
+//
+//	var left, right *ListNode = pre, nil
+//	if this.length%2 != 0 {
+//		right = mid.next
+//	} else {
+//		right = mid
+//	}
+//	for nil != left && nil != right {
+//		if left.GetValue().(string) != right.GetValue().(string) {
+//			isPalindrome = false
+//			break
+//		}
+//		left = left.next
+//		right = right.next
+//	}
+//
+//	// 还原链表
+//	cur = pre
+//	pre = mid
+//	for nil != cur {
+//		tmp := cur.next
+//		cur.next = pre
+//		pre = cur
+//		cur = tmp
+//	}
+//	this.head.next = pre
+//	return isPalindrome
+//}
+//
+//// 思路二：申请一个栈存储链表前半段，时间复杂度O(n)，空间复杂度O(n)
+//func (this *LinkedList) isPalindrome2() bool {
+//	len := this.length
+//	switch len {
+//	case 0:
+//		return false
+//	case 1:
+//		return true
+//	}
+//	str := make([]string, 0, len/2)
+//	cur := this.head
+//	for i := uint(1); i <= len; i++ {
+//		cur = cur.next
+//		if len%2 != 0 && i == (len/2+1) { // 如果链表有奇数个节点，忽略中间节点
+//			continue
+//		}
+//		if i <= len/2 { // 前半段入栈，后半段对比
+//			str = append(str, cur.value.(string))
+//		} else {
+//			if str[len-i] != cur.value.(string) {
+//				return false
+//			}
+//		}
+//	}
+//	return true
+//}
+//
+//// 翻转链表
+//func (this *LinkedList) ReverseLinkedList() {
+//	if nil == this.head || nil == this.head.next || nil == this.head.next.next {
+//		return
+//	}
+//	pre := this.head.next
+//	cur := this.head.next.next
+//	pre.next = nil
+//	for nil != cur {
+//		tmp := cur.next
+//		cur.next = pre
+//		pre = cur
+//		cur = tmp
+//	}
+//	this.head.next = pre
+//}
+//
+//// 判断链表是否有环
+//// 方法一： 双重遍历，时间复杂度O(n*n)，空间复杂度O(1)
+//func (this *LinkedList) hasCycle() bool {
+//	cur := this.head.next
+//	for count := int(1); nil != cur; count++ {
+//		new_cur := this.head.next
+//		for new_count := 1; new_count < count; new_count++ {
+//			if new_cur == cur {
+//				return true
+//			}
+//			new_cur = new_cur.next
+//		}
+//		cur = cur.next
+//	}
+//	return false
+//}
+//
+//// 方法二：HashSet存储，时间复杂度O(n)，空间复杂度O(n)
+//
+//// 方法三：快慢指针，时间复杂度O(n)，空间复杂度O(1)
+//func (this *LinkedList) hasCycle2() bool {
+//	slow := this.head.next
+//	fast := this.head.next
+//	for nil != fast && nil != fast.next {
+//		slow = slow.next
+//		fast = fast.next.next
+//		if fast == slow {
+//			return true
+//		}
+//	}
+//	return false
+//}
+//
+//// 获取有环链表入环点
+//func (this *LinkedList) getEntryNodeOfLoop() interface{} {
+//	slow := this.head.next
+//	fast := this.head.next
+//	for nil != fast && nil != fast.next {
+//		slow = slow.next
+//		fast = fast.next.next
+//		if fast == slow {
+//			break
+//		}
+//	}
+//	// 相遇后慢指针返回链表头
+//	slow = this.head.next
+//	for slow != fast {
+//		slow = slow.next
+//		fast = fast.next
+//	}
+//	return slow
+//}
+//
+//// 获取有环链表环长
+//func (this *LinkedList) getCycleLengthOfLoop() uint {
+//	slow := this.head.next
+//	fast := this.head.next
+//	for nil != fast && nil != fast.next {
+//		slow = slow.next
+//		fast = fast.next.next
+//		if slow == fast {
+//			break
+//		}
+//	}
+//	slow = slow.next
+//	fast = fast.next.next
+//	var length uint = 1
+//	for slow != fast {
+//		slow = slow.next
+//		fast = fast.next.next
+//		length++
+//	}
+//	return length
+//}
+//
+//// 判断两个链表是否相交，以及获取第一个交点
+//// 分有环和无环
+//func hasIntersection(linked_list1 *LinkedList, linked_list2 *LinkedList) interface{} {
+//	// 一个有环，一个无环，必然不相交
+//	// 如果都是无环，判断最后一个节点是否相同
+//	if !linked_list1.hasCycle2() && !linked_list2.hasCycle2() {
+//		end1 := linked_list1.head.next
+//		end2 := linked_list2.head.next
+//		for end1.next != nil {
+//			end1 = end1.next
+//		}
+//		for end2.next != nil {
+//			end2 = end2.next
+//		}
+//		if end1 == end2 {
+//			return true
+//		}
+//		return false
+//	} else if linked_list1.hasCycle2() && linked_list2.hasCycle2() {
+//		// 都有环，必然是同一个环，则判断A中的入环点是否在B中出现过
+//		entryNode := linked_list1.getEntryNodeOfLoop()
+//		cur := linked_list2.head.next
+//		for cur != nil {
+//			if cur == entryNode {
+//				return true
+//			}
+//			cur = cur.next
+//		}
+//		return false
+//	} else {
+//		return false
+//	}
+//}
--- kit/02_linkedlist/SingleLinkedList_test.go
+++ kit/02_linkedlist/SingleLinkedList_test.go
@@ -5,208 +5,256 @@ import (
 	"testing"
 )
 
-func TestInsertToHead(t *testing.T) {
+func TestLinkedList_InsertToHead(t *testing.T) {
 	l := NewLinkedList()
-	for i := 0; i < 10; i++ {
-		l.InsertToHead(i * i)
+	total := 10
+	fmt.Printf("添加一个长度为%d的链表\n", total)
+	for i:= 0; i < total; i++ {
+		l.InsertToTail(NewListNode(i * i))
 	}
+	l.Print()
+	newValue := 10
+	fmt.Printf("头插法插入%d\n", newValue)
+	l.InsertToHead(NewListNode(newValue))
 	l.Print()
 }
 
-func TestInsertToTail(t *testing.T) {
+func TestLinkedList_InsertToTail(t *testing.T) {
 	l := NewLinkedList()
-	str := "hello"
-	for _, v := range str {
-		l.InsertToTail(string(v))
+	total := 10
+	fmt.Printf("添加一个长度为%d的链表\n", total)
+	for i:= 0; i < total; i++ {
+		l.InsertToTail(NewListNode(i * i))
 	}
+	l.Print()
+	newValue := 10
+	fmt.Printf("头插法插入%d\n", newValue)
+	l.InsertToTail(NewListNode(newValue))
 	l.Print()
 }
 
-func TestInsertBefore(t *testing.T) {
+func TestLinkedList_FindNodeByIndex(t *testing.T) {
 	l := NewLinkedList()
-	for i := 0; i < 10; i++ {
-		l.InsertToHead(i * i)
-	}
-	oldNode := NewListNode("oldNode")
-	l.InsertToHead(oldNode)
-	l.Print()
-}
-
-func TestInsertAfter(t *testing.T) {
-
-}
-
-func TestFindByIndex(t *testing.T) {
-	l := NewLinkedList()
-	for i := 0; i < 10; i++ {
-		l.InsertToHead(i * i)
-	}
-	t.Log(l.FindByIndex(2))
-}
-
-func TestDeleteNode(t *testing.T) {
-	l := NewLinkedList()
-	for i := 0; i < 3; i++ {
-		l.InsertToTail(i + 1)
+	total := 10
+	fmt.Printf("添加一个长度为%d的链表\n", total)
+	for i:= 0; i < total; i++ {
+		l.InsertToTail(NewListNode(i * i))
 	}
 	l.Print()
 
-	t.Log(l.DeleteNode(l.head.next))
-	l.Print()
-
-	t.Log(l.DeleteNode(l.head.next.next))
-	l.Print()
+	newIndex := uint(9)
+	fmt.Printf("查找索引为%d的节点\n", newIndex)
+	fmt.Println(l.FindNodeByIndex(newIndex).GetValue())
 }
 
-func TestReverseLinkedList(t *testing.T) {
+func TestLinkedList_InsertAfterNode(t *testing.T) {
 	l := NewLinkedList()
-	str := "hello"
-	for _, v := range str {
-		l.InsertToTail(string(v))
+	total := 10
+	fmt.Printf("添加一个长度为%d的链表\n", total)
+	for i:= 0; i < total; i++ {
+		l.InsertToTail(NewListNode(i * i))
 	}
 	l.Print()
-	l.ReverseLinkedList()
+
+	newIndex := uint(5)
+	fmt.Printf("查找索引为%d的节点\n", newIndex)
+	oldNode := l.FindNodeByIndex(newIndex)
+	fmt.Println(oldNode.GetValue())
+
+	newValue := 666
+	fmt.Printf("后面添加值为%d的新节点\n", newValue)
+	l.InsertAfterNode(oldNode, NewListNode(newValue))
 	l.Print()
 }
 
-func TestIsPalindrome(t *testing.T) {
+func TestLinkedList_InsertBeforeNode(t *testing.T) {
 	l := NewLinkedList()
-	str := "helloworld"
-	for _, v := range str {
-		l.InsertToTail(string(v))
+	total := 10
+	fmt.Printf("添加一个长度为%d的链表\n", total)
+	for i:= 0; i < total; i++ {
+		l.InsertToTail(NewListNode(i * i))
 	}
 	l.Print()
-	t.Log(l.IsPalindrome())
+
+	newIndex := uint(5)
+	fmt.Printf("查找索引为%d的节点\n", newIndex)
+	oldNode := l.FindNodeByIndex(newIndex)
+	fmt.Println(oldNode.GetValue())
+
+	newValue := 666
+	fmt.Printf("前面添加值为%d的新节点\n", newValue)
+	l.InsertBeforeNode(oldNode, NewListNode(newValue))
 	l.Print()
 }
 
-func TestIsPalindrome2(t *testing.T) {
+func TestLinkedList_DeleteNode(t *testing.T) {
 	l := NewLinkedList()
-	str := "helleh"
-	for _, v := range str {
-		l.InsertToTail(string(v))
+	total := 10
+	fmt.Printf("添加一个长度为%d的链表\n", total)
+	for i:= 0; i < total; i++ {
+		l.InsertToTail(NewListNode(i * i))
 	}
 	l.Print()
-	t.Log(l.IsPalindrome())
+
+	newIndex := uint(5)
+	fmt.Printf("查找索引为%d的节点\n", newIndex)
+	oldNode := l.FindNodeByIndex(newIndex)
+	fmt.Println(oldNode.GetValue())
+
+	fmt.Println("删除该节点")
+	l.DeleteNode(oldNode)
 	l.Print()
 }
 
-func TestHasCyCle(t *testing.T) {
-	node1 := NewListNode(1)
-	node2 := NewListNode(2)
-	node3 := NewListNode(3)
-	node4 := NewListNode(4)
-	node5 := NewListNode(5)
-	node6 := NewListNode(6)
-	node7 := NewListNode(7)
-	node1.next = node2
-	node2.next = node3
-	node3.next = node4
-	node4.next = node5
-	node5.next = node6
-	node6.next = node7
-	node7.next = node4
-
-	linked_list := NewLinkedList()
-	linked_list.head.next = node1
-
-	t.Log(linked_list.hasCycle())
-	fmt.Println()
-}
-
-func TestHasCyCle2(t *testing.T) {
-	node1 := NewListNode(1)
-	node2 := NewListNode(2)
-	node3 := NewListNode(3)
-	node4 := NewListNode(4)
-	node5 := NewListNode(5)
-	node6 := NewListNode(6)
-	node7 := NewListNode(7)
-	node1.next = node2
-	node2.next = node3
-	node3.next = node4
-	node4.next = node5
-	node5.next = node6
-	node6.next = node7
-	node7.next = node4
-
-	linked_list := NewLinkedList()
-	linked_list.head.next = node1
-
-	t.Log(linked_list.hasCycle2())
-}
-
-func TestGetEntryNodeOfLoop(t *testing.T) {
-	node1 := NewListNode(1)
-	node2 := NewListNode(2)
-	node3 := NewListNode(3)
-	node4 := NewListNode(4)
-	node5 := NewListNode(5)
-	node6 := NewListNode(6)
-	node7 := NewListNode(7)
-	node1.next = node2
-	node2.next = node3
-	node3.next = node4
-	node4.next = node5
-	node5.next = node6
-	node6.next = node7
-	node7.next = node4
-
-	linked_list := NewLinkedList()
-	linked_list.head.next = node1
-	t.Log(linked_list.getEntryNodeOfLoop())
-}
-
-func TestGetCycleLengthOfLoop(t *testing.T) {
-	node1 := NewListNode(1)
-	node2 := NewListNode(2)
-	node3 := NewListNode(3)
-	node4 := NewListNode(4)
-	node5 := NewListNode(5)
-	node6 := NewListNode(6)
-	node7 := NewListNode(7)
-	node1.next = node2
-	node2.next = node3
-	node3.next = node4
-	node4.next = node5
-	node5.next = node6
-	node6.next = node7
-	node7.next = node4
-
-	linked_list := NewLinkedList()
-	linked_list.head.next = node1
-	t.Log(linked_list.getCycleLengthOfLoop())
-}
-
-func TestHasIntersection(t *testing.T) {
-	node1 := NewListNode(1)
-	node2 := NewListNode(2)
-	node3 := NewListNode(3)
-	node4 := NewListNode(4)
-	node5 := NewListNode(5)
-	node6 := NewListNode(6)
-	node7 := NewListNode(7)
-	node1.next = node2
-	node2.next = node3
-	node3.next = node4
-	node4.next = node5
-	node5.next = node6
-	node6.next = node7
-	// node7.next = node4 // 有环1->2->3->4->5->6->7->4
-
-	linked_list := NewLinkedList()
-	linked_list.head.next = node1
-
-	node8 := NewListNode(8) // 8-9->10->6
-	node9 := NewListNode(9)
-	node10 := NewListNode(10)
-	node8.next = node9
-	node9.next = node10
-	node10.next = node6
-	linked_list2 := NewLinkedList()
-	linked_list2.head.next = node8
-
-	// linked_list.Print()
-	// linked_list2.Print()
-	t.Log(hasIntersection(linked_list, linked_list2))
-}
+//func TestReverseLinkedList(t *testing.T) {
+//	l := NewLinkedList()
+//	str := "hello"
+//	for _, v := range str {
+//		l.InsertToTail(string(v))
+//	}
+//	l.Print()
+//	l.ReverseLinkedList()
+//	l.Print()
+//}
+//
+//func TestIsPalindrome(t *testing.T) {
+//	l := NewLinkedList()
+//	str := "helloworld"
+//	for _, v := range str {
+//		l.InsertToTail(string(v))
+//	}
+//	l.Print()
+//	t.Log(l.IsPalindrome())
+//	l.Print()
+//}
+//
+//func TestIsPalindrome2(t *testing.T) {
+//	l := NewLinkedList()
+//	str := "helleh"
+//	for _, v := range str {
+//		l.InsertToTail(string(v))
+//	}
+//	l.Print()
+//	t.Log(l.IsPalindrome())
+//	l.Print()
+//}
+//
+//func TestHasCyCle(t *testing.T) {
+//	node1 := NewListNode(1)
+//	node2 := NewListNode(2)
+//	node3 := NewListNode(3)
+//	node4 := NewListNode(4)
+//	node5 := NewListNode(5)
+//	node6 := NewListNode(6)
+//	node7 := NewListNode(7)
+//	node1.next = node2
+//	node2.next = node3
+//	node3.next = node4
+//	node4.next = node5
+//	node5.next = node6
+//	node6.next = node7
+//	node7.next = node4
+//
+//	linked_list := NewLinkedList()
+//	linked_list.head.next = node1
+//
+//	t.Log(linked_list.hasCycle())
+//	fmt.Println()
+//}
+//
+//func TestHasCyCle2(t *testing.T) {
+//	node1 := NewListNode(1)
+//	node2 := NewListNode(2)
+//	node3 := NewListNode(3)
+//	node4 := NewListNode(4)
+//	node5 := NewListNode(5)
+//	node6 := NewListNode(6)
+//	node7 := NewListNode(7)
+//	node1.next = node2
+//	node2.next = node3
+//	node3.next = node4
+//	node4.next = node5
+//	node5.next = node6
+//	node6.next = node7
+//	node7.next = node4
+//
+//	linked_list := NewLinkedList()
+//	linked_list.head.next = node1
+//
+//	t.Log(linked_list.hasCycle2())
+//}
+//
+//func TestGetEntryNodeOfLoop(t *testing.T) {
+//	node1 := NewListNode(1)
+//	node2 := NewListNode(2)
+//	node3 := NewListNode(3)
+//	node4 := NewListNode(4)
+//	node5 := NewListNode(5)
+//	node6 := NewListNode(6)
+//	node7 := NewListNode(7)
+//	node1.next = node2
+//	node2.next = node3
+//	node3.next = node4
+//	node4.next = node5
+//	node5.next = node6
+//	node6.next = node7
+//	node7.next = node4
+//
+//	linked_list := NewLinkedList()
+//	linked_list.head.next = node1
+//	t.Log(linked_list.getEntryNodeOfLoop())
+//}
+//
+//func TestGetCycleLengthOfLoop(t *testing.T) {
+//	node1 := NewListNode(1)
+//	node2 := NewListNode(2)
+//	node3 := NewListNode(3)
+//	node4 := NewListNode(4)
+//	node5 := NewListNode(5)
+//	node6 := NewListNode(6)
+//	node7 := NewListNode(7)
+//	node1.next = node2
+//	node2.next = node3
+//	node3.next = node4
+//	node4.next = node5
+//	node5.next = node6
+//	node6.next = node7
+//	node7.next = node4
+//
+//	linked_list := NewLinkedList()
+//	linked_list.head.next = node1
+//	t.Log(linked_list.getCycleLengthOfLoop())
+//}
+//
+//func TestHasIntersection(t *testing.T) {
+//	node1 := NewListNode(1)
+//	node2 := NewListNode(2)
+//	node3 := NewListNode(3)
+//	node4 := NewListNode(4)
+//	node5 := NewListNode(5)
+//	node6 := NewListNode(6)
+//	node7 := NewListNode(7)
+//	node1.next = node2
+//	node2.next = node3
+//	node3.next = node4
+//	node4.next = node5
+//	node5.next = node6
+//	node6.next = node7
+//	// node7.next = node4 // 有环1->2->3->4->5->6->7->4
+//
+//	linked_list := NewLinkedList()
+//	linked_list.head.next = node1
+//
+//	node8 := NewListNode(8) // 8-9->10->6
+//	node9 := NewListNode(9)
+//	node10 := NewListNode(10)
+//	node8.next = node9
+//	node9.next = node10
+//	node10.next = node6
+//	linked_list2 := NewLinkedList()
+//	linked_list2.head.next = node8
+//
+//	// linked_list.Print()
+//	// linked_list2.Print()
+//	t.Log(hasIntersection(linked_list, linked_list2))
+//}

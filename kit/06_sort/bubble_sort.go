b37e0d0960106ac5892a33387b3be27d52c6e616
pengwenwu <pww932589183@163.com>
2020-05-08 23:16:57 +0800

    add:冒泡排序

--- kit/06_sort/bubble_sort.go
+++ kit/06_sort/bubble_sort.go
@@ -1,20 +1,25 @@
 package sort
 
 // 冒泡、插入、选择排序
-func BubbleSort(arr []int, n int) {
+func BubbleSort(arr []int, n int) []int {
 	if n <= 1 {
-		return
+		return arr
 	}
 	for i := 0; i < n; i++ {
 		// 提前结束标识，没有交换则已经全部有序
 		flag := false
-		for j:= i; j < n; j++ {
-			if arr[i] > arr[j] {
-				tmp := arr[j]
-				arr[j] = arr[i]
-				arr[i] = tmp
+		for j:= 0; j < n - i - 1; j++ {
+			if arr[j] > arr[j+1] {
+				tmp := arr[j+1]
+				arr[j+1] = arr[j]
+				arr[j] = tmp
 				flag = true
 			}
 		}
+		// 没有交换
+		if !flag {
+			break
+		}
 	}
+	return arr
 }

1f90df371ebae90d3233875cebd208f21dea7125
pengwenwu <pww932589183@163.com>
2020-05-09 00:05:28 +0800

    add:插入排序

--- kit/06_sort/bubble_sort.go
+++ kit/06_sort/bubble_sort.go
@@ -8,6 +8,7 @@ func BubbleSort(arr []int, n int) []int {
 	for i := 0; i < n; i++ {
 		// 提前结束标识，没有交换则已经全部有序
 		flag := false
+		// 每次循环，最大的数必然在末尾
 		for j:= 0; j < n - i - 1; j++ {
 			if arr[j] > arr[j+1] {
 				tmp := arr[j+1]
@@ -20,6 +21,25 @@ func BubbleSort(arr []int, n int) []int {
 		if !flag {
 			break
 		}
+	}
+	return arr
+}
+
+func InsertionSort(arr []int, n int) []int {
+	if n <= 1 {
+		return arr
+	}
+	for i := 1; i < n; i++ {
+		value := arr[i]
+		j := i - 1
+		for ; j > 0; j-- {
+			if arr[j] > value {
+				arr[j+1] = arr[j]
+ 			} else {
+ 				break
+			}
+ 		}
+ 		arr[j+1] = value
 	}
 	return arr
 }
--- kit/06_sort/bubble_sort_test.go
+++ kit/06_sort/bubble_sort_test.go
@@ -20,3 +20,17 @@ func TestBubbleSort(t *testing.T) {
 	tc := time.Since(startT)
 	fmt.Printf("time cost = %v\n", tc)
 }
+
+func TestInsertionSort(t *testing.T) {
+	const n = 10000
+	var arr []int
+	arr = make([]int, n)
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < n; i++ {
+		arr[i] = rand.Intn(1000)
+	}
+	startT := time.Now()
+	InsertionSort(arr, n)
+	tc := time.Since(startT)
+	fmt.Printf("time cost = %v\n", tc)
+}

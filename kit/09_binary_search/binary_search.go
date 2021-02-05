c5a2986224badb929a823903fe1d8372febde6c5
pengwenwu <pengwenwu@corp-ci.com>
2021-02-05 14:25:14 +0800

    add:二分查找binary_search

--- kit/09_binary_search/binary_search.go
+++ kit/09_binary_search/binary_search.go
@@ -44,3 +44,127 @@ func bs(arr []int, findVal, low ,high int) int {
         return bs(arr, findVal, mid+1, high)
     }
 }
+
+// 二分查找第一个值等于给定值的元素
+func BinarySearchFirst(arr []int, findVal int) int {
+    num := len(arr)
+    if num == 0 {
+        return -1
+    }
+    low := 0
+    high := num - 1
+    for low <= high {
+        mid := (low + high) / 2
+        if arr[mid] >= findVal {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    if low < num && arr[low] == findVal {
+        return low
+    } else {
+        return -1
+    }
+    
+    return 0
+}
+
+func BinarySearchFirstSimple(arr []int, findVal int) int {
+    num := len(arr)
+    if num == 0 {
+        return -1
+    }
+
+    low := 0
+    high := num - 1
+    for low <= high {
+        mid := (low + high) / 2
+        if arr[mid] > findVal {
+            high = mid - 1
+        } else if arr[mid] < findVal {
+            low = mid + 1
+        } else {
+            if mid == 0 || arr[mid-1] != findVal {
+                return mid
+            } else {
+                high = mid - 1
+            }
+        }
+    }
+    return -1
+}
+
+func BinarySearchLast(arr []int, findVal int) int {
+    num := len(arr)
+    if num == 0 {
+        return -1
+    }
+
+    low := 0
+    high := num - 1
+    for low <= high {
+        mid := (low + high) / 2
+        if arr[mid] > findVal {
+            high = mid - 1
+        } else if arr[mid] < findVal {
+            low = mid + 1
+        } else {
+            if mid == num - 1 || arr[mid+1] != findVal {
+                return mid
+            } else {
+                low = mid + 1
+            }
+        }
+    }
+    return -1
+}
+
+// 查找第一个大于等于给定值的元素
+func BinarySearchFirstGT(arr []int, findVal int) int {
+    num := len(arr)
+    if num == 0 {
+        return -1
+    }
+
+    low := 0
+    high := num - 1
+    for low <= high {
+        mid := low + (high - low) / 2
+        if arr[mid] >= findVal {
+            if mid == 0 || arr[mid-1] < findVal {
+                return mid
+            } else {
+                high = mid - 1
+            }
+        } else {
+            low = mid + 1
+        }
+    }
+    return -1
+}
+
+// 查找最后一个小于等于给定值的元素
+func BinarySearchLastLT(arr []int, findVal int) int {
+    num := len(arr)
+    if num == 0 {
+        return -1
+    }
+
+    low := 0
+    high := num - 1
+    for low <= high {
+        mid := low + (high - low) / 2
+        if arr[mid] > findVal {
+            high = mid - 1
+        } else {
+            if mid == num - 1 || arr[mid+1] > findVal {
+                return mid
+            } else {
+                low = mid + 1
+            }
+        }
+    }
+
+    return -1
+}
--- kit/09_binary_search/binary_search_test.go
+++ kit/09_binary_search/binary_search_test.go
@@ -25,3 +25,103 @@ func TestBinarySearchRecursive(t *testing.T) {
         t.Fatal(BinarySearchRecursive(a, 4))
     }
 }
+
+func TestBinarySearchFirst(t *testing.T) {
+    var a []int
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirst(a, 2) != 1 {
+        t.Fatal(BinarySearchFirst(a, 2))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirst(a, 3) != 4 {
+        t.Fatal(BinarySearchFirst(a, 3))
+    }
+}
+
+func TestBinarySearchFirstSimple(t *testing.T) {
+    var a []int
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirstSimple(a, 2) != 1 {
+        t.Fatal(BinarySearchFirstSimple(a, 2))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirstSimple(a, 3) != 4 {
+        t.Fatal(BinarySearchFirstSimple(a, 3))
+    }
+}
+
+func TestBinarySearchLast(t *testing.T) {
+    var a []int
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchLast(a, 2) != 3 {
+        t.Fatal(BinarySearchLast(a, 2))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchLast(a, 3) != 4 {
+        t.Fatal(BinarySearchLast(a, 3))
+    }
+}
+
+func TestBinarySearchFirstGT(t *testing.T) {
+    var a []int
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirstGT(a, 2) != 1 {
+        t.Fatal(BinarySearchFirstGT(a, 2))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirstGT(a, 1) != 0 {
+        t.Fatal(BinarySearchFirstGT(a, 1))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirstGT(a, 3) != 4 {
+        t.Fatal(BinarySearchFirstGT(a, 3))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirstGT(a, 4) != 5 {
+        t.Fatal(BinarySearchFirstGT(a, 4))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchFirstGT(a, 5) != -1 {
+        t.Fatal(BinarySearchFirstGT(a, 5))
+    }
+}
+
+func TestBinarySearchLastLT(t *testing.T) {
+    var a []int
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchLastLT(a, 2) != 3 {
+        t.Fatal(BinarySearchLastLT(a, 2))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchLastLT(a, 1) != 0 {
+        t.Fatal(BinarySearchLastLT(a, 1))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchLastLT(a, 3) != 4 {
+        t.Fatal(BinarySearchLastLT(a, 3))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchLastLT(a, 4) != 5 {
+        t.Fatal(BinarySearchLastLT(a, 4))
+    }
+
+    a = []int{1, 2, 2, 2, 3, 4}
+    if BinarySearchLastLT(a, 0) != -1 {
+        t.Fatal(BinarySearchLastLT(a, 0))
+    }
+}
